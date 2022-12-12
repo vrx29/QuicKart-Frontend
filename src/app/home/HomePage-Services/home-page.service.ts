5abc9f27b4c9e03018ba0ad9f903eb941f21b336
Siddharth Dwivedi <siddharthdwivedi318@gmail.com>
2022-12-12 20:04:26 +0530

    Sample Version

--- src/app/home/HomePage-Services/home-page.service.ts
+++ src/app/home/HomePage-Services/home-page.service.ts
@@ -20,15 +20,16 @@ export class HomePageService {
 
   //Getting the Products from backend API
   getProducts():Observable<IProduct[]>{
-    let tempVar = this.http.get<IProduct[]>('https://localhost:5001/api/home/getproducts')
+    let tempVar = this.http.get<IProduct[]>('https://quickkart-backend.azurewebsites.net/api/home/getproducts')
     console.log(tempVar)
     return tempVar
   }
 
   PostNewSubscriber(emailID:string):Observable<boolean>{
   
+    console.log(emailID)
 
-    let tempVar = this.http.get<boolean>('https://localhost:5001/AddNewSubscriber?emailID='+emailID)
+    let tempVar = this.http.get<boolean>('https://quickkart-backend.azurewebsites.net/api/customer/AddNewSubscriber?emailID='+emailID)
     console.log(tempVar)
     return tempVar
   }
@@ -39,7 +40,7 @@ export class HomePageService {
     var user:User
     user={emailID:userEmailID, password:userPassword,usertype:type};
     console.log(user)
-    let result=this.http.post<number>('https://quickcart-functionapp.azurewebsites.net/api/LoginFunction?code=kDxJUURhVektI3q0G7_k0zea7O9yKG0FG-VfWfLqbLzpAzFuZJP6ww==',user)
+    let result=this.http.post<number>('https://quickkart-backend.azurewebsites.net/api/LoginFunction?code=kDxJUURhVektI3q0G7_k0zea7O9yKG0FG-VfWfLqbLzpAzFuZJP6ww==',user)
     return result
 
   }
